35f72655cbc0ab5d90b724be6251139021dff5a2
MoatasemTAlmasani <78753417+MoatasemTAlmasani@users.noreply.github.com>
2021-02-15 16:43:23 +0300

    Add files via upload

---
+++ RepeatNO.java
@@ -0,0 +1,28 @@
+
+
+public class RepeatNO {
+    static int findRepeating(int arr[], int n)
+    {
+
+        int res = 0;
+        for (int i = 0; i < n - 1; i++)
+            res = res ^ (i + 1) ^ arr[i];
+        res = res ^ arr[n - 1];
+
+        return res;
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = { 9, 8,8, 2, 6, 1, 8, 5,5, 3, 4,  };
+        int n = arr.length;
+        System.out.println(findRepeating(arr, n));
+    }
+}
+
+
+
+
+
+
+
+
